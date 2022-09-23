8e277daddf729f147562827df9ceeaee30ce69c8
axelaguirre02 <axelgaston02@gmail.com>
2022-09-23 00:20:57 -0300

    Mini proyecto finalizado

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,25 @@
+import { TaskContext } from "../context/TaskContext";
+import { useContext } from "react";
+import TaskCard from "./TaskCard";
+
+function TaskList() {
+  const { tasks } = useContext(TaskContext);
+
+  if (tasks.length === 0) {
+    return (
+      <h1 className="text-white text-4xl font-bold text-center">
+        Aun no hay tareas 🤷‍♂️
+      </h1>
+    );
+  }
+
+  return (
+    <div className="grid grid-cols-4 gap-2">
+      {tasks.map((task) => (
+        <TaskCard key={task.id} task={task} />
+      ))}
+    </div>
+  );
+}
+
+export default TaskList;
---
+++ src/data/tasks.js
@@ -0,0 +1,22 @@
+export const tasks = [
+  {
+    id: 1,
+    title: "Primera Tarea",
+    description: "Descripcion de la primera tarea",
+  },
+  {
+    id: 2,
+    title: "Segunda Tarea",
+    description: "Descripcion de la segunda tarea",
+  },
+  {
+    id: 3,
+    title: "Tercera Tarea",
+    description: "Descripcion de la tercera tarea",
+  },
+  {
+    id: 4,
+    title: "Cuarta Tarea",
+    description: "Descripcion de la cuarta tarea",
+  },
+];
